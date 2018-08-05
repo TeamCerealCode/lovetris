24e54f687eeef42f0cfd3b568292dbf088ca2ea9
TheFox <krakaka088@gmail.com>
2018-08-05 08:06:13 +0300

    no idea how git works, hotfix (?)

--- main.lua
+++ main.lua
@@ -5,7 +5,6 @@ local height = love.graphics.getHeight()
 local displaydebug = false
 local windowcenterx = width / 2
 local windowcentery = height / 2
-<<<<<<< HEAD
 
 local tilesize = 20
 
@@ -27,30 +26,19 @@ n = 0
 for n = 0, gridysize-1, 1 do
     table.insert(gridy, #gridy+1, gridx)
 end
-=======
->>>>>>> master
 
 function love.draw()
     -- grid draw
     love.graphics.setColor(1, 1, 1, 1)
-<<<<<<< HEAD
     for n = 0, tilesize * gridxsize, tilesize do
         love.graphics.line(windowcenterx - tilesize * gridxsize / 2 + n, windowcentery - tilesize * gridysize / 2, windowcenterx - tilesize * gridxsize / 2 + n, windowcentery + tilesize * gridysize / 2)
     end
     for i = 0, tilesize * gridysize, tilesize do
         love.graphics.line(windowcenterx - tilesize * gridxsize / 2, windowcentery - tilesize * gridysize / 2 + i, windowcenterx + tilesize * gridxsize / 2, windowcentery - tilesize * gridysize / 2 + i)
-=======
-	for n = 1, 220, 20 do
-        love.graphics.line(windowcenterx-100+n, windowcentery-200, windowcenterx-100+n, windowcentery+200)
-    end
-	for n = 1, 420, 20 do
-        love.graphics.line(windowcenterx-100, windowcentery-200+n, windowcenterx+100, windowcentery-200+n)
->>>>>>> master
     end
 
     -- debug menu. use if you want to test something instead of littering code !!!!
     if displaydebug then
-<<<<<<< HEAD
         love.graphics.setColor(1, 1, 1, 0.9) 
         -- note to self: tostring()
         love.graphics.print('render options:')
@@ -59,15 +47,11 @@ function love.draw()
         love.graphics.print('tile size: '..tostring(tilesize), 0, 45)
         love.graphics.print('misc variables and such:', 0, 60) 
         love.graphics.print('none', 0, 75) 
-=======
-        love.graphics.setColor(1, 1, 1, 0.9)
-        -- note to self: tostring()
-        love.graphics.print('nothing atm') 
->>>>>>> master
     end
 end
 
 function love.update(dt)
+
 end
 
 function love.keypressed(key)
@@ -77,17 +61,9 @@ function love.keypressed(key)
     end
 end
 
-<<<<<<< HEAD
 function love.resize(w, h) 
     width = w 
     height = h 
     windowcenterx = width / 2 
     windowcentery = height / 2 
-=======
-function love.resize(w, h)
-    width = w
-    height = h
-    windowcenterx = width / 2
-    windowcentery = height / 2
->>>>>>> master
 end
