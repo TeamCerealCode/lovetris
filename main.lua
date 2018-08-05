e8868e04f6e7386481b85dd28643dbc412c25a03
TheFox <krakaka088@gmail.com>
2018-08-05 06:38:02 +0300

    grid drawing!! + debug menu
    the grid now draws! i also added in a debug menu at f3 to debug the game if something goes wrong

--- main.lua
+++ main.lua
@@ -1,9 +1,43 @@
---m
+-- variables n stuff
+width = love.graphics.getWidth( display )
+height = love.graphics.getHeight( display )
+
+windowcenterx = width/2
+windowcentery = height/2
+
+-- set default launch things like title etc
+love.window.setTitle("lovetris")
+
 
 function love.draw()
+    -- grid draw
+    love.graphics.setColor(1,1,1,1)
+    n = 0
+    while n<220 do
+        love.graphics.line(windowcenterx-100+n, windowcentery-200, windowcenterx-100+n, windowcentery+200)
+        n = n + 20
+    end
+    i = 0
+    while i<420 do
+        love.graphics.line(windowcenterx-100, windowcentery-200+i, windowcenterx+100, windowcentery-200+i)
+        i = i + 20
+    end
 
+    -- debug menu. use if you want to test something instead of littering code !!!!
+    if displaydebug then
+        love.graphics.setColor(1,1,1,0.9)
+        --note to self: tostring()
+        love.graphics.print('nothing atm', 0, 0) 
+    end
 end
 
 function love.update(dt)
 
+end
+
+function love.keypressed(key,scancode,isrepeat)
+    -- debug menu
+    if key == 'f3' then
+        if displaydebug then displaydebug = false else displaydebug = true end
+    end
 end
