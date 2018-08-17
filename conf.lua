c01af5edd3616dffa631e557696026e332b4d15e
TheFox <krakaka088@gmail.com>
2018-08-17 14:48:42 +0300

    misc conf.lua changes

--- conf.lua
+++ conf.lua
@@ -1,7 +1,10 @@
 function love.conf(t)
+    t.identity = "lovetrisConfig"
+    t.version = "11.1"
+    t.release = false
     t.window.title = 'lovetris'
     t.window.resizable = true
     t.window.vsync = false
-    t.window.minwidth = 450
+    t.window.minwidth = 575
     t.window.minheight = 510 
 end
